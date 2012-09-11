f1094bf0b74a668f7f802fc00f1426452bc3462d
Shaunak Kishore <skishore@mit.edu>
2012-09-11 00:19:24 -0700

    Connect to server via socket

--- app.js
+++ app.js
@@ -17,34 +17,29 @@ web_server.listen(3000, function () {
               'http://' + addr.address + ':' + addr.port);
 });
 
-///**
-// * Socket.IO server (single process only)
-// */
-//
-//var io = sio.listen(app)
-//  , nicknames = {};
-//
-//io.sockets.on('connection', function (socket) {
-//  socket.on('user message', function (msg) {
-//    socket.broadcast.emit('user message', socket.nickname, msg);
-//  });
-//
-//  socket.on('nickname', function (nick, fn) {
-//    if (nicknames[nick]) {
-//      fn(true);
-//    } else {
-//      fn(false);
-//      nicknames[nick] = socket.nickname = nick;
-//      socket.broadcast.emit('announcement', nick + ' connected');
-//      io.sockets.emit('nicknames', nicknames);
-//    }
-//  });
-//
-//  socket.on('disconnect', function () {
-//    if (!socket.nickname) return;
-//
-//    delete nicknames[socket.nickname];
-//    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-//    socket.broadcast.emit('nicknames', nicknames);
-//  });
-//});
+var io = sio.listen(web_server, {log: false})
+var nicknames = {};
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function(msg) {
+    socket.broadcast.emit('message', socket.nickname, msg);
+  });
+
+  socket.on('nickname', function(nick, fn) {
+    if (nicknames[nick]) {
+      fn(true);
+    } else {
+      fn(false);
+      nicknames[nick] = socket.nickname = nick;
+      socket.broadcast.emit('announcement', nick + ' connected');
+      io.sockets.emit('nicknames', nicknames);
+    }
+  });
+
+  socket.on('disconnect', function() {
+    if (!socket.nickname) return;
+
+    delete nicknames[socket.nickname];
+    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+    socket.broadcast.emit('nicknames', nicknames);
+  });
+});
--- static/script.js
+++ static/script.js
@@ -1,25 +1,8 @@
-function queryServer(query, returnCall) {
-  pingServer(query, 'query', returnCall);
-}
+var socket;
 
-function updateServer(update, returnCall) {
-  pingServer(update, 'update', returnCall);
-}
-
-function pingServer(query, queryType, returnCall) {
-  if (typeof(returnCall) == 'undefined') {
-    returnCall = function() {};
-  }
-  var request = $.get;
-  if (queryType == 'update') {
-    request = $.post;
-  }
-  request(queryType, query, function(json) {
-    autoParseJson(json);
-    returnCall(json);
-  }, 'xml');
-}
-
-function autoParseJson(json) {
-
-}
+$(document).ready(function() {
+  socket = io.connect();
+  socket.on('connect', function() {
+    console.debug('Socket connected');
+  });
+});
