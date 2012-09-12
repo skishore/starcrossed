126a25cbfe0ab0172817321bba1a77548b6b3420
Shaunak Kishore <skishore@mit.edu>
2012-09-12 00:09:28 -0700

    Add status div

--- static/script.js
+++ static/script.js
@@ -8,12 +8,20 @@ $(document).ready(function() {
   socket = io.connect();
 
   socket.on('connect', function() {
-    console.debug('Socket connected');
+    $('#status').html('Connected!');
+    $('#status').removeClass('waiting disconnected');
+    $('#status').addClass('connected');
   });
 
   socket.on('pid', function(message) {
     result = JSON.parse(message);
     console.debug(uid);
     console.debug(result);
+  });
+
+  socket.on('disconnect', function() {
+    $('#status').html('Disconnected. The server is probably down.');
+    $('#status').removeClass('waiting connected');
+    $('#status').addClass('disconnected');
   });
 });
